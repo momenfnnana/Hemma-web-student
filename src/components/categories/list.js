a3316f92a1c2ddb393d6246eadbeb3a6e8a63694
Sarah Abuteen <sarahabuteen@jawabra.home>
2019-03-24 11:18:01 +0200

    Content loader in categories list

--- src/components/categories/list.js
+++ src/components/categories/list.js
@@ -2,20 +2,43 @@ import React, { Component } from "react";
 import axios from "axios";
 import { CardsList } from "../shared/cardsList/cardsList";
 import { Link } from "react-router-dom";
+import ContentLoader from "react-content-loader";
 
+const MyLoader = props => (
+  <ContentLoader
+    rtl
+    height={50}
+    width={300}
+    speed={2}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+    {...props}
+  >
+    <circle cx="25" cy="25" r="25" />
+    <rect x="60" y="21" rx="4" ry="4" width="150" height="10" />
+  </ContentLoader>
+);
 export class Categories extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories: []
+      categories: [],
+      loading: false
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true });
     axios
       .get("https://api.staging.hemma.sa/api/v1/categories?limit=40")
       .then(response => {
         this.setState({ categories: response.data.data.data });
+        setTimeout(
+          function() {
+            this.setState({ loading: false });
+          }.bind(this),
+          800
+        );
       })
       .catch(error => {
         console.log(error);
@@ -28,12 +51,21 @@ export class Categories extends Component {
       <React.Fragment>
         <div className="row pt-5">
           <div className="col-12 d-inline-flex align-items-center">
-            <Link to={`/categories/details/${cat.id}`} key={cat.id}>
-              <h5 key={cat.id} className="dark-text mb-0">
-                <img key={cat.id} src={cat.icon} height="50" className="mr-2" />
-                {cat.nameAr}
-              </h5>
-            </Link>
+            {this.state.loading ? (
+              <MyLoader style={{ height: 50 }} />
+            ) : (
+              <Link to={`/categories/details/${cat.id}`} key={cat.id}>
+                <h5 key={cat.id} className="dark-text mb-0">
+                  <img
+                    key={cat.id}
+                    src={cat.icon}
+                    height="50"
+                    className="mr-2"
+                  />
+                  {cat.nameAr}
+                </h5>
+              </Link>
+            )}
           </div>
         </div>
         <div className="row pt-4">
--- src/components/shared/cardsList/cardsList.js
+++ src/components/shared/cardsList/cardsList.js
@@ -4,16 +4,40 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Card } from "../card/card";
+import ContentLoader from "react-content-loader";
+
+const MyLoader = props => (
+  <ContentLoader
+    rtl
+    height={300}
+    width={300}
+    speed={2}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+    {...props}
+  >
+    <rect x="5" y="71.95" rx="5" ry="5" width="250" height="0" />
+    <rect x="9" y="190" rx="4" ry="4" width="148" height="10" />
+    <rect x="9" y="211" rx="4" ry="4" width="275" height="10" />
+    <rect x="9" y="7.61" rx="0" ry="0" width="275" height="172.33" />
+    <rect x="9" y="233" rx="4" ry="4" width="70" height="10" />
+    <rect x="113" y="234" rx="4" ry="4" width="70" height="10" />
+    <rect x="214" y="235" rx="4" ry="4" width="70" height="10" />
+    <rect x="215" y="257" rx="4" ry="4" width="70" height="10" />
+  </ContentLoader>
+);
 
 export class CardsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      courses: []
+      courses: [],
+      loading: false
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true });
     axios
       .get(
         "https://api.staging.hemma.sa/api/v1/categories/" +
@@ -22,6 +46,12 @@ export class CardsList extends Component {
       )
       .then(response => {
         this.setState({ courses: response.data.data.data });
+        setTimeout(
+          function() {
+            this.setState({ loading: false });
+          }.bind(this),
+          800
+        );
       })
       .catch(error => {
         console.log(error);
@@ -30,7 +60,13 @@ export class CardsList extends Component {
 
   renderCards() {
     return this.state.courses.map(course => (
-      <Card key={course.id} course={course} />
+      <React.Fragment>
+        {this.state.loading ? (
+          <MyLoader />
+        ) : (
+          <Card key={course.id} course={course} />
+        )}
+      </React.Fragment>
     ));
   }
 
