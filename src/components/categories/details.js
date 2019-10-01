ddba148082c571d728cbe1bd24c052c47770e34c
Sarah Abuteen <sabuteen@terkwaz.com>
2019-10-01 12:57:03 +0300

    Free lectures in categories

--- src/components/categories/details.js
+++ src/components/categories/details.js
@@ -46,18 +46,15 @@ export class CategoryDetails extends Component {
         console.log(error);
       });
 
-    // axios
-    //   .get(
-    //     `${apiBaseUrl}/categories/${
-    //       params.id
-    //     }/free-lectures`
-    //   )
-    //   .then(response => {
-    //     this.setState({ lectures: response.data.data.data });
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //   });
+    axios
+      .get(`${apiBaseUrl}/FreeLectures?categoryIdOrSlug=${params.slug}`)
+      .then(response => {
+        this.setState({ lectures: response.data.data });
+        console.log("lectures ", this.state.lectures);
+      })
+      .catch(error => {
+        console.log(error);
+      });
 
     // axios
     //   .get(
@@ -128,46 +125,44 @@ export class CategoryDetails extends Component {
   renderCards() {
     return this.state.courses.map(course => (
       <React.Fragment>
-        <div className="col-4">
+        <div className="col-md-4">
           <Card key={course.id} course={course} />
         </div>
       </React.Fragment>
     ));
   }
 
-  // renderLectures() {
-  //   return this.state.lectures.map(lecture => (
-  //     <li
-  //       key={lecture.id}
-  //       className="list-group-item d-flex justify-content-between align-items-center"
-  //       dir="rtl"
-  //     >
-  //       <div className="media">
-  //         <div className="gradient-bg mr-4 d-flex align-items-center justify-content-center">
-  //           <FaGraduationCap className="text-white" size="34" />
-  //         </div>
-  //         <div className="media-body mt-2">
-  //           <h6 className="dark-text mb-0">{lecture.nameAr}</h6>
-  //           <ul className="list-inline mt-2">
-  //             <li className="list-inline-item small mt-0">
-  //               <p href="" className="light-text">
-  //                 أ. طلال محمد{" "}
-  //               </p>
-  //             </li>
-  //             <li className="list-inline-item light-font-text small mt-0 ml-2">
-  //               <div className="dark-bg pl-4 pr-4 pt-1 pb-1 rounded">
-  //                 <p className="text-white en-text mb-0">10:54</p>
-  //               </div>
-  //             </li>
-  //           </ul>
-  //         </div>
-  //       </div>
-  //       <button type="submit" className="btn light-outline-btn unset-height">
-  //         انضم
-  //       </button>
-  //     </li>
-  //   ));
-  // }
+  renderLectures() {
+    return this.state.lectures.map(lecture => (
+      <li
+        key={lecture.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+        dir="rtl"
+      >
+        <div className="media d-flex align-items-center">
+          <div className="gradient-bg mr-4 d-flex align-items-center justify-content-center">
+            <FaGraduationCap className="text-white" size="34" />
+          </div>
+          <div className="media-body">
+            <h6 className="dark-text">{lecture.nameAr}</h6>
+            {lecture.instructor && (
+              <p className="light-text small mb-0">
+                {lecture.instructor && lecture.instructor.name}
+              </p>
+            )}
+          </div>
+        </div>
+        {lecture.broadcastUrl && (
+          <button
+            className="btn light-outline-btn unset-height"
+            onClick={() => window.open(lecture.broadcastUrl, "_blank")}
+          >
+            انضم
+          </button>
+        )}
+      </li>
+    ));
+  }
 
   // renderFreeContent() {
   //   return this.state.courses.map(course => (
@@ -241,40 +236,40 @@ export class CategoryDetails extends Component {
       ]
     };
 
-    // var verticalCarousel = {
-    //   infinite: false,
-    //   slidesToShow: 3,
-    //   slidesToScroll: 1,
-    //   autoplay: false,
-    //   autoplaySpeed: 2000,
-    //   arrows: true,
-    //   vertical: true,
-    //   responsive: [
-    //     {
-    //       breakpoint: 1024,
-    //       settings: {
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1,
-    //         infinite: false
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 600,
-    //       settings: {
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1,
-    //         initialSlide: 1
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 480,
-    //       settings: {
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1
-    //       }
-    //     }
-    //   ]
-    // };
+    var verticalCarousel = {
+      infinite: false,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      arrows: true,
+      vertical: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
 
     const {
       match: { params }
@@ -301,14 +296,14 @@ export class CategoryDetails extends Component {
             </div>
             <div className="row pt-2 pb-5">{this.renderCards()}</div>
 
-            {/* {this.state.content && this.state.content.length > 0 ? (
-              <div className="row pt-5 pb-3">
+            {this.state.lectures && this.state.lectures.length > 0 ? (
+              <div className="row pt-2 pb-3">
                 <div className="col-md-4">
-                  <h3 className="dark-text mt-3">
-                    إننا حابين نفيدكم قدمنا لكم
-                  </h3>
-                  <h3 className="light-text">محاضرات مجانية</h3>
-                  <p className="dark-silver-text small">
+                  <h4 className="dark-text mt-3">
+                    لأننا حابين نفيدكم قدمنا لكم
+                  </h4>
+                  <h4 className="light-text">محاضرات مجانية</h4>
+                  <p className="dark-silver-text small text-break mb-0">
                     نقدم مجموعة من المحاضرات المجانية كل أسبوعتابعونا لتعرفوا
                     المزيد
                   </p>
@@ -317,7 +312,7 @@ export class CategoryDetails extends Component {
                   <Slider {...verticalCarousel}>{this.renderLectures()}</Slider>
                 </div>
               </div>
-            ) : null} */}
+            ) : null}
 
             {/* <div className="row pt-5 pb-3 no-gutters d-flex align-items-center">
               <div className="col-md-8 col-12">
