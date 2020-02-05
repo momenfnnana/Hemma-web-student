e60fe9a66d1d58febcee2cff50956bfd94a2d72b
Sarah Abuteen <sabuteen@terkwaz.com>
2020-02-06 00:27:49 +0200

    Render subscriptions based on status

--- src/components/account/subscriptions/subscriptions.js
+++ src/components/account/subscriptions/subscriptions.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Tooltip } from "reactstrap";
+import {
+  Tooltip,
+  Nav,
+  NavItem,
+  NavLink,
+  Table,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  TabContent,
+  TabPane
+} from "reactstrap";
 import { Link, withRouter } from "react-router-dom";
 import { apiBaseUrl } from "../../../api/helpers";
 import swal from "@sweetalert/with-react";
 import { connect } from "react-redux";
 import { getProfile } from "../../../actions";
+import classnames from "classnames";
 import Loader from "react-loaders";
 import "loaders.css/src/animations/ball-clip-rotate.scss";
 
@@ -23,8 +35,14 @@ export class SubscriptionsComponent extends Component {
       hideBtn: false,
       loading: false,
       disabled: false,
-      nextPageUrl: `${apiBaseUrl}/courses/purchased?Page=${this.page}&Limit=${this.limit}`
+      nextPageUrl: `${apiBaseUrl}/courses/purchased?Page=${this.page}&Limit=${this.limit}`,
+      activeTab: "Active"
     };
+    this.setActiveTab = this.setActiveTab.bind(this);
+  }
+
+  setActiveTab(tab) {
+    this.setState({ activeTab: tab });
   }
 
   toggle() {
@@ -75,87 +93,82 @@ export class SubscriptionsComponent extends Component {
     const subscriptions = this.state.subscriptions || [];
     return subscriptions.map(subscription => (
       <React.Fragment>
-        <div
-          key={subscription.id}
-          className="bg-white box-layout w-100 p-3 d-flex align-items-center mb-4 clickable"
-          onClick={() => {
-            subscription.cumulativePaymentStatus == "Unpaid" ||
-            // subscription.cumulativePaymentStatus == "PartiallyPaid" ||
-            subscription.cumulativePaymentStatus == "Pending"
-              ? swal(
-                  "عفواً",
-                  "تفاصيل الدورة غير متاحة حتى يتم مراجعة الحوالة من قبل الإدارة",
-                  "error",
-                  {
-                    button: "متابعة"
-                  }
-                )
-              : subscription.subscriptionStatus == "Expired"
-              ? swal(
-                  "عفواً",
-                  "تفاصيل الدورة غير متاحة بسبب انتهاء الاشتراك",
-                  "error",
-                  {
-                    button: "متابعة"
-                  }
-                )
-              : this.props.history.push(
-                  `/subscriptions/${subscription.course.id}`
-                );
-          }}
-        >
-          <div className="media w-75">
-            <img
-              className="mr-3 rounded cover-img"
-              src={subscription.course.bannerUrl}
-              height="100"
-              width="100"
-            />
-            <div className="media-body mt-2">
-              <h6 className="mt-0 dark-text">{subscription.course.nameAr}</h6>
-              {subscription.subscriptionStatus == "Cancelled" ? (
-                <span className="badge blue-status light-font-text">ملغية</span>
-              ) : subscription.subscriptionStatus == "Expired" ? (
-                <span className="badge blue-status light-font-text">
-                  انتهى الاشتراك
-                </span>
-              ) : null}
+        {this.state.activeTab == subscription.subscriptionStatus && (
+          <div className="col-md-4 col-12">
+            <div
+              key={subscription.id}
+              onClick={() => {
+                subscription.cumulativePaymentStatus == "Unpaid" ||
+                // subscription.cumulativePaymentStatus == "PartiallyPaid" ||
+                subscription.cumulativePaymentStatus == "Pending"
+                  ? swal(
+                      "عفواً",
+                      "تفاصيل الدورة غير متاحة حتى يتم مراجعة الحوالة من قبل الإدارة",
+                      "error",
+                      {
+                        button: "متابعة"
+                      }
+                    )
+                  : subscription.subscriptionStatus == "Expired"
+                  ? swal(
+                      "عفواً",
+                      "تفاصيل الدورة غير متاحة بسبب انتهاء الاشتراك",
+                      "error",
+                      {
+                        button: "متابعة"
+                      }
+                    )
+                  : this.props.history.push(
+                      `/subscriptions/${subscription.course.id}`
+                    );
+              }}
+            >
+              <div className="card card-sm md-height shadow-sm border-0 clickable">
+                <header className="card-thumb">
+                  <img src={subscription.course.bannerUrl} alt="Course image" />
+                </header>
+                <div className="card-body d-flex flex-column justify-content-center">
+                  <h6 className="card-title small mb-0 p-0 dark-text">
+                    {subscription.course.nameAr}
+                  </h6>
+
+                  {subscription.cumulativePaymentStatus == "Unpaid" ? (
+                    <p className="dark-silver-text small mb-0">غير مسدد</p>
+                  ) : subscription.cumulativePaymentStatus ==
+                    "PartiallyPaid" ? (
+                    <p className="dark-silver-text small mb-0">مسدد جزئياً</p>
+                  ) : subscription.cumulativePaymentStatus == "FullyPaid" ? (
+                    <p className="dark-silver-text small mb-0">مسدد</p>
+                  ) : subscription.cumulativePaymentStatus == "Pending" ? (
+                    <React.Fragment>
+                      <p
+                        className="dark-silver-text small mb-0"
+                        id="status-tooltip"
+                      >
+                        قيد المراجعة
+                      </p>
+                      <Tooltip
+                        placement="right"
+                        isOpen={this.state.tooltipOpen}
+                        target="status-tooltip"
+                        toggle={this.toggle}
+                        placement="bottom"
+                        style={{
+                          backgroundColor: "#f2fdfe",
+                          color: "#4b3a85"
+                        }}
+                      >
+                        <p className="light-font-text small mb-1 mt-2">
+                          تم تلقي الحوالة وسوف يتم الموافقة عليها خلال 48 ساعة
+                        </p>
+                      </Tooltip>
+                    </React.Fragment>
+                  ) : null}
+                </div>
+              </div>
             </div>
           </div>
-          <div className="seperator" />
-          <div className="">
-            <h6 className="dark-text mb-0 small">الحالة المالية</h6>
-
-            {subscription.cumulativePaymentStatus == "Unpaid" ? (
-              <p className="dark-silver-text small mb-0">غير مسدد</p>
-            ) : subscription.cumulativePaymentStatus == "PartiallyPaid" ? (
-              <p className="dark-silver-text small mb-0">مسدد جزئياً</p>
-            ) : subscription.cumulativePaymentStatus == "FullyPaid" ? (
-              <p className="dark-silver-text small mb-0">مسدد</p>
-            ) : subscription.cumulativePaymentStatus == "Pending" ? (
-              <React.Fragment>
-                <p className="dark-silver-text small mb-0" id="status-tooltip">
-                  قيد المراجعة
-                </p>
-                <Tooltip
-                  placement="right"
-                  isOpen={this.state.tooltipOpen}
-                  target="status-tooltip"
-                  toggle={this.toggle}
-                  placement="bottom"
-                  style={{
-                    backgroundColor: "#f2fdfe",
-                    color: "#4b3a85"
-                  }}
-                >
-                  <p className="light-font-text small mb-1 mt-2">
-                    تم تلقي الحوالة وسوف يتم الموافقة عليها خلال 48 ساعة
-                  </p>
-                </Tooltip>
-              </React.Fragment>
-            ) : null}
-          </div>
-        </div>
+        )}
       </React.Fragment>
     ));
   }
@@ -175,7 +188,7 @@ export class SubscriptionsComponent extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-4">
-                <div className="white-bg box-layout w-100 p-4 d-flex align-items-center justify-content-center flex-column  mb-4">
+                <div className="white-bg box-layout w-100 p-4 d-flex align-items-center justify-content-center flex-column mb-4">
                   <img src={avatarImg} height="110" className="mb-3" />
                   <h6 className="dark-text mb-1">
                     {this.props.initialValues && this.props.initialValues.name}
@@ -196,7 +209,7 @@ export class SubscriptionsComponent extends Component {
                 </div>
               </div>
               <div className="col-md-8">
-                <h3 className="dark-text">قائمة دوراتي</h3>
+                <h5 className="dark-text mb-3">قائمة دوراتي</h5>
 
                 {this.state.subscriptions == undefined ||
                 this.state.subscriptions.length == 0 ? (
@@ -216,91 +229,102 @@ export class SubscriptionsComponent extends Component {
                   </React.Fragment>
                 ) : (
                   <React.Fragment>
-                    <div className="silver-bg box-layout w-100 pb-0 p-4 mt-4">
-                      {this.renderCourses()}
-                    </div>
-                    {!this.state.hideBtn && (
-                      <div className="d-flex align-items-center justify-content-center">
-                        <button
-                          className="btn dark-btn unset-height unset-line-height br-5 mt-3 w-25"
-                          onClick={this.loadMore}
-                          disabled={this.state.disabled}
+                    <Nav tabs className="account-tabs mx-auto">
+                      <NavItem>
+                        <NavLink
+                          className={classnames({
+                            active: this.state.activeTab === "Active"
+                          })}
+                          onClick={() => this.setActiveTab("Active")}
                         >
-                          {this.state.loading == true ? (
-                            <Loader type="ball-clip-rotate" />
-                          ) : (
-                            "تحميل المزيد"
-                          )}
-                        </button>
-                      </div>
-                    )}
+                          سارية
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({
+                            active: this.state.activeTab === "Expired"
+                          })}
+                          onClick={() => this.setActiveTab("Expired")}
+                        >
+                          منتهية
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({
+                            active: this.state.activeTab === "Withdrawn"
+                          })}
+                          onClick={() => this.setActiveTab("Withdrawn")}
+                        >
+                          منسحب
+                        </NavLink>
+                      </NavItem>
+                    </Nav>
+                    <TabContent activeTab={this.state.activeTab}>
+                      <TabPane tabId="Active">
+                        <div className="silver-bg box-layout w-100 pb-0 p-3 mt-4">
+                          <div className="row">{this.renderCourses()}</div>
+                        </div>
+                        {!this.state.hideBtn && (
+                          <div className="d-flex align-items-center justify-content-center">
+                            <button
+                              className="btn dark-btn unset-height unset-line-height br-5 mt-3 w-25"
+                              onClick={this.loadMore}
+                              disabled={this.state.disabled}
+                            >
+                              {this.state.loading == true ? (
+                                <Loader type="ball-clip-rotate" />
+                              ) : (
+                                "تحميل المزيد"
+                              )}
+                            </button>
+                          </div>
+                        )}
+                      </TabPane>
+                      <TabPane tabId="Expired">
+                        <div className="silver-bg box-layout w-100 pb-0 p-3 mt-4">
+                          <div className="row">{this.renderCourses()}</div>
+                        </div>
+                        {!this.state.hideBtn && (
+                          <div className="d-flex align-items-center justify-content-center">
+                            <button
+                              className="btn dark-btn unset-height unset-line-height br-5 mt-3 w-25"
+                              onClick={this.loadMore}
+                              disabled={this.state.disabled}
+                            >
+                              {this.state.loading == true ? (
+                                <Loader type="ball-clip-rotate" />
+                              ) : (
+                                "تحميل المزيد"
+                              )}
+                            </button>
+                          </div>
+                        )}
+                      </TabPane>
+                      <TabPane tabId="Withdrawn">
+                        <div className="silver-bg box-layout w-100 pb-0 p-3 mt-4">
+                          <div className="row">{this.renderCourses()}</div>
+                        </div>
+                        {!this.state.hideBtn && (
+                          <div className="d-flex align-items-center justify-content-center">
+                            <button
+                              className="btn dark-btn unset-height unset-line-height br-5 mt-3 w-25"
+                              onClick={this.loadMore}
+                              disabled={this.state.disabled}
+                            >
+                              {this.state.loading == true ? (
+                                <Loader type="ball-clip-rotate" />
+                              ) : (
+                                "تحميل المزيد"
+                              )}
+                            </button>
+                          </div>
+                        )}
+                      </TabPane>
+                    </TabContent>
                   </React.Fragment>
                 )}
-
-                {/* <div className="bg-white box-layout w-100 p-3 d-flex align-items-center mb-4">
-                    <div className="media w-75">
-                      <img
-                        className="mr-3 rounded cover-img"
-                        src={
-                          process.env.PUBLIC_URL + "/assets/images/course2.png"
-                        }
-                        height="100"
-                        width="100"
-                      />
-                      <div className="media-body mt-2">
-                        <h6 className="mt-0 dark-text">
-                          دورة القدرات للجامعيين
-                        </h6>
-
-                        <span
-                          className="badge yellow-status light-font-text tooltip-on-hover"
-                          id="status-tooltip"
-                        >
-                          قيد المراجعة
-                        </span>
-
-                        <Tooltip
-                          placement="right"
-                          isOpen={this.state.tooltipOpen}
-                          target="status-tooltip"
-                          toggle={this.toggle}
-                          placement="bottom"
-                          style={{
-                            backgroundColor: "#f2fdfe",
-                            color: "#4b3a85"
-                          }}
-                        >
-                          <p className="light-font-text small mb-1 mt-2">
-                            حقك علينا لسه ما تم تأكيد طلبك إذا تجاوز طلبك 48
-                            ساعة ياريت تراسلنا
-                          </p>
-                          <p
-                            className="small en-text mb-2 d-inline-flex align-items-center"
-                            dir="ltr"
-                          >
-                            <img
-                              src={
-                                process.env.PUBLIC_URL +
-                                "/assets/images/whatsapp.png"
-                              }
-                              height="20"
-                              width="20"
-                              className="ml-1"
-                            />
-                            +9660539412412{" "}
-                          </p>
-                        </Tooltip>
-                      </div>
-                    </div>
-                    <div className="seperator" />
-                    <div className="">
-                      <h6 className="dark-text mb-0 small">الحالة المالية</h6>
-                      <p className="dark-silver-text small mb-0">
-                        مسددة جزئيا{" "}
-                      </p>
-                    </div>
-                  </div> */}
-                {/* </div> */}
               </div>
             </div>
           </div>
