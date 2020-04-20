1ecface3bb0a4c9616738dd6efb896d32d29a6f5
Sarah Abuteen <sabuteen@terkwaz.com>
2020-04-20 22:38:57 +0300

    Internal and extrnal urls chat fix

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -25,52 +25,74 @@ export class UsersChatComponent extends Component {
   render() {
     return (
       <React.Fragment>
-        {this.props.chatChannelSid.startsWith("http") && (
-          <div className="row">
-            <div className="col-md-12">
-              <div
-                className="chat-title border h-55 d-flex align-items-center justify-content-center mb-4 rounded shadow-sm clickable"
-                onClick={() => window.open(this.props.chatChannelSid, "_blank")}
-              >
-                <h6 className="media chat-item mb-0 d-flex align-items-center light-text small">
-                  رابط مجموعة التيليجرام
-                </h6>
-              </div>
+        {!this.props.chatEnabled ? (
+          <React.Fragment>
+            <div className="chat-title border h-55 d-flex align-items-center justify-content-center mb-4 rounded shadow-sm clickable">
+              <h6 className="media chat-item mb-0 d-flex align-items-center light-text small">
+                الدردشة غير فعالة
+              </h6>
             </div>
-          </div>
-        )}
-
-        <div className="box-layout shadow-sm w-100">
-          <div className="row no-gutters">
-            <div className="chat-sidebar col-md-4 border-right">
-              <div className="p-3">
-                <input
-                  className="form-control small light-font-text"
-                  placeholder="ابحث هنا"
-                />
-              </div>
-              {this.props.chatChannelSid && (
-                <ChatList
-                  courseId={this.props.courseId}
-                  generalChatId={this.props.chatChannelSid}
-                />
-              )}
-            </div>
-            <div className="col-md-8">
-              <div className="chat-title border-bottom h-55 d-flex align-items-center justify-content-center">
-                <h6 className="dark-text small mb-0">دردشة للجميع</h6>
-              </div>
-              <div className="chat-window">
-                <div className="chat-history">
-                  <ul className="list-unstyled">
-                    <MessagesList />
-                  </ul>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            {!this.props.forceInternalChat ? (
+              <div className="row">
+                <div className="col-md-12">
+                  {this.props.externalChannelUrl ? (
+                    <div
+                      className="chat-title border h-55 d-flex align-items-center justify-content-center mb-4 rounded shadow-sm clickable"
+                      onClick={() =>
+                        window.open(this.props.externalChannelUrl, "_blank")
+                      }
+                    >
+                      <h6 className="media chat-item mb-0 d-flex align-items-center light-text small">
+                        رابط مجموعة التيليجرام
+                      </h6>
+                    </div>
+                  ) : (
+                    <div className="chat-title border h-55 d-flex align-items-center justify-content-center mb-4 rounded shadow-sm clickable">
+                      <h6 className="media chat-item mb-0 d-flex align-items-center light-text small">
+                        لم يتم إضافة الرابط
+                      </h6>
+                    </div>
+                  )}
                 </div>
-                <MessageInput />
               </div>
-            </div>
-          </div>
-        </div>
+            ) : (
+              <div className="box-layout shadow-sm w-100">
+                <div className="row no-gutters">
+                  <div className="chat-sidebar col-md-4 border-right">
+                    <div className="p-3">
+                      <input
+                        className="form-control small light-font-text"
+                        placeholder="ابحث هنا"
+                      />
+                    </div>
+                    {this.props.internalChannelId && (
+                      <ChatList
+                        courseId={this.props.courseId}
+                        generalChatId={this.props.internalChannelId}
+                      />
+                    )}
+                  </div>
+                  <div className="col-md-8">
+                    <div className="chat-title border-bottom h-55 d-flex align-items-center justify-content-center">
+                      <h6 className="dark-text small mb-0">دردشة للجميع</h6>
+                    </div>
+                    <div className="chat-window">
+                      <div className="chat-history">
+                        <ul className="list-unstyled">
+                          <MessagesList />
+                        </ul>
+                      </div>
+                      <MessageInput />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </React.Fragment>
+        )}
       </React.Fragment>
     );
   }
