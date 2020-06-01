88755f91bdf87af7e6509127f375daa0e91bf2d4
Saif Abu Shamma <sabushamma@terkwaz.com>
2020-06-01 17:53:40 +0300

    STUD-108 Hide discussions

--- src/components/account/subscriptions/discussions/discussion-details.js
+++ src/components/account/subscriptions/discussions/discussion-details.js
@@ -1,122 +1,122 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import { getUser } from "../../../../actions/user.actions";
-import { getChatToken } from "../../../../actions/twilio.actions";
-import { UsersChatComponent } from "../../../chat/chat";
-import { apiBaseUrl } from "../../../../api/helpers";
-import { reduxForm } from "redux-form";
-import axios from "axios";
-import "./styles.sass";
+// import React, { Component } from "react";
+// import { withRouter } from "react-router-dom";
+// import { connect } from "react-redux";
+// import { getUser } from "../../../../actions/user.actions";
+// import { getChatToken } from "../../../../actions/twilio.actions";
+// import { UsersChatComponent } from "../../../chat/chat";
+// import { apiBaseUrl } from "../../../../api/helpers";
+// import { reduxForm } from "redux-form";
+// import axios from "axios";
+// import "./styles.sass";
 
-export class DiscussionDetailsComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      discussionDetails: []
-    };
-  }
+// export class DiscussionDetailsComponent extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       discussionDetails: []
+//     };
+//   }
 
-  async componentDidMount() {
-    const discussionId = this.props.match.params.discussionId;
-    let token = localStorage.getItem("token");
-    let headers = {
-      Authorization: `Bearer ${token}`
-    };
-    axios
-      .get(`${apiBaseUrl}/discussions/${discussionId}`, { headers })
-      .then(response => {
-        this.setState({ discussionDetails: response.data.data });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+//   async componentDidMount() {
+//     const discussionId = this.props.match.params.discussionId;
+//     let token = localStorage.getItem("token");
+//     let headers = {
+//       Authorization: `Bearer ${token}`
+//     };
+//     axios
+//       .get(`${apiBaseUrl}/discussions/${discussionId}`, { headers })
+//       .then(response => {
+//         this.setState({ discussionDetails: response.data.data });
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       });
+//   }
 
-  render() {
-    const courseId = this.props.match.params.id;
-    const startsAt = new Date(
-      this.state.discussionDetails && this.state.discussionDetails.startsAt
-    );
-    const startDate = startsAt.toLocaleString();
-    const endsAt = new Date(
-      this.state.discussionDetails && this.state.discussionDetails.endsAt
-    );
-    const endDate = endsAt.toLocaleString();
+//   render() {
+//     const courseId = this.props.match.params.id;
+//     const startsAt = new Date(
+//       this.state.discussionDetails && this.state.discussionDetails.startsAt
+//     );
+//     const startDate = startsAt.toLocaleString();
+//     const endsAt = new Date(
+//       this.state.discussionDetails && this.state.discussionDetails.endsAt
+//     );
+//     const endDate = endsAt.toLocaleString();
 
-    return (
-      <React.Fragment>
-        <div className="row no-gutters">
-          <div className="col-12">
-            <div className="box-layout shadow-sm mb-4">
-              <div className="discussion-item pt-4 unset-height">
-                <div className="media d-flex align-items-center mb-3">
-                  <div className="media-body">
-                    <h6 className="dark-text">
-                      {this.state.discussionDetails &&
-                        this.state.discussionDetails.title}
-                    </h6>
-                    <div className="d-flex align-items-center">
-                      <h6 className="dark-silver-text smaller mb-0 mr-3">
-                        من:{" "}
-                        <span className="en-text" dir="ltr">
-                          {startDate}
-                        </span>
-                      </h6>
-                      <h6 className="dark-silver-text smaller mb-0">
-                        إلى:{" "}
-                        <span className="en-text" dir="ltr">
-                          {endDate}
-                        </span>
-                      </h6>
-                    </div>
-                  </div>
-                  <div className="d-flex flex-column align-items-end justify-content-end">
-                    {this.state.discussionDetails &&
-                    this.state.discussionDetails.active == true ? (
-                      <span className="badge light-bg text-white">مفتوح</span>
-                    ) : (
-                      <span className="badge red-bg text-white">مغلق</span>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {this.state.discussionDetails &&
-          this.state.discussionDetails.chatChannelSid && (
-            <UsersChatComponent
-              title="عنوان المناقشة"
-              internalChannelId={this.state.discussionDetails.chatChannelSid}
-              courseId={courseId}
-              chatEnabled={true}
-              forceInternalChat={true}
-              allowSend={this.state.discussionDetails.active}
-            />
-          )}
-      </React.Fragment>
-    );
-  }
-}
+//     return (
+//       <React.Fragment>
+//         <div className="row no-gutters">
+//           <div className="col-12">
+//             <div className="box-layout shadow-sm mb-4">
+//               <div className="discussion-item pt-4 unset-height">
+//                 <div className="media d-flex align-items-center mb-3">
+//                   <div className="media-body">
+//                     <h6 className="dark-text">
+//                       {this.state.discussionDetails &&
+//                         this.state.discussionDetails.title}
+//                     </h6>
+//                     <div className="d-flex align-items-center">
+//                       <h6 className="dark-silver-text smaller mb-0 mr-3">
+//                         من:{" "}
+//                         <span className="en-text" dir="ltr">
+//                           {startDate}
+//                         </span>
+//                       </h6>
+//                       <h6 className="dark-silver-text smaller mb-0">
+//                         إلى:{" "}
+//                         <span className="en-text" dir="ltr">
+//                           {endDate}
+//                         </span>
+//                       </h6>
+//                     </div>
+//                   </div>
+//                   <div className="d-flex flex-column align-items-end justify-content-end">
+//                     {this.state.discussionDetails &&
+//                     this.state.discussionDetails.active == true ? (
+//                       <span className="badge light-bg text-white">مفتوح</span>
+//                     ) : (
+//                       <span className="badge red-bg text-white">مغلق</span>
+//                     )}
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         {this.state.discussionDetails &&
+//           this.state.discussionDetails.chatChannelSid && (
+//             <UsersChatComponent
+//               title="عنوان المناقشة"
+//               internalChannelId={this.state.discussionDetails.chatChannelSid}
+//               courseId={courseId}
+//               chatEnabled={true}
+//               forceInternalChat={true}
+//               allowSend={this.state.discussionDetails.active}
+//             />
+//           )}
+//       </React.Fragment>
+//     );
+//   }
+// }
 
-function mapStateToProps(state) {
-  return {
-    formValues:
-      state.form.DiscussionDetails && state.form.DiscussionDetails.values,
-    authenticated: state.auth.authenticated,
-    user: state.user,
-    twilio: state.twilio
-  };
-}
+// function mapStateToProps(state) {
+//   return {
+//     formValues:
+//       state.form.DiscussionDetails && state.form.DiscussionDetails.values,
+//     authenticated: state.auth.authenticated,
+//     user: state.user,
+//     twilio: state.twilio
+//   };
+// }
 
-DiscussionDetailsComponent = reduxForm({
-  form: "DiscussionDetails"
-})(DiscussionDetailsComponent);
+// DiscussionDetailsComponent = reduxForm({
+//   form: "DiscussionDetails"
+// })(DiscussionDetailsComponent);
 
-DiscussionDetailsComponent = connect(
-  mapStateToProps,
-  { getUser, getChatToken }
-)(DiscussionDetailsComponent);
+// DiscussionDetailsComponent = connect(
+//   mapStateToProps,
+//   { getUser, getChatToken }
+// )(DiscussionDetailsComponent);
 
-export const DiscussionDetails = withRouter(DiscussionDetailsComponent);
+// export const DiscussionDetails = withRouter(DiscussionDetailsComponent);
--- src/components/account/subscriptions/discussions/discussions-list.js
+++ src/components/account/subscriptions/discussions/discussions-list.js
@@ -1,137 +1,137 @@
-import React, { Component } from "react";
-import "./styles.sass";
-import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { apiBaseUrl } from "../../../../api/helpers";
-import Loader from "react-loaders";
-import "loaders.css/src/animations/ball-spin-fade-loader.scss";
+// import React, { Component } from "react";
+// import "./styles.sass";
+// import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+// import { apiBaseUrl } from "../../../../api/helpers";
+// import Loader from "react-loaders";
+// import "loaders.css/src/animations/ball-spin-fade-loader.scss";
 
-export class DiscussionsList extends Component {
-  state = {
-    discussions: [],
-    isPageLoading: false
-  };
+// export class DiscussionsList extends Component {
+//   state = {
+//     discussions: [],
+//     isPageLoading: false
+//   };
 
-  componentDidMount() {
-    this.setState({ isPageLoading: true });
-    const courseId = this.props.match.params.id;
+//   componentDidMount() {
+//     this.setState({ isPageLoading: true });
+//     const courseId = this.props.match.params.id;
 
-    let token = localStorage.getItem("token");
-    let headers = {
-      Authorization: `Bearer ${token}`
-    };
-    axios
-      .get(`${apiBaseUrl}/discussions?courseId=${courseId}`, { headers })
-      .then(response => {
-        this.setState({ discussions: response.data.data, isPageLoading: false });
-      })
-      .catch(error => {
-        this.setState({ isPageLoading: false });
-        console.log(error);
-      });
-  }
+//     let token = localStorage.getItem("token");
+//     let headers = {
+//       Authorization: `Bearer ${token}`
+//     };
+//     axios
+//       .get(`${apiBaseUrl}/discussions?courseId=${courseId}`, { headers })
+//       .then(response => {
+//         this.setState({ discussions: response.data.data, isPageLoading: false });
+//       })
+//       .catch(error => {
+//         this.setState({ isPageLoading: false });
+//         console.log(error);
+//       });
+//   }
 
-  renderDiscussions() {
-    const discussions = this.state.discussions;
-    const courseId = this.props.match.params.id;
+//   renderDiscussions() {
+//     const discussions = this.state.discussions;
+//     const courseId = this.props.match.params.id;
 
-    if (discussions) {
-      return discussions.map(discussion => {
-        const startsAt = new Date(discussion.startsAt);
-        const startDate = startsAt.toLocaleString();
-        const endsAt = new Date(discussion.endsAt);
-        const endDate = endsAt.toLocaleString();
-        return (
-          <Link
-            to={`/course/content/${courseId}/discussions/${discussion.id}`}
-            className="discussion-item d-flex align-items-center"
-          >
-            <div
-              className="media w-90 d-flex align-items-center justify-content-between"
-              key={discussion.id}
-            >
-              <div className="media-body">
-                <h6 className="dark-text">{discussion.title}</h6>
-                <div className="d-flex align-items-center">
-                  <h6 className="dark-silver-text smaller mb-0 mr-3">
-                    من:{" "}
-                    <span className="en-text" dir="ltr">
-                      {startDate}
-                    </span>
-                  </h6>
-                  <h6 className="dark-silver-text smaller mb-0">
-                    إلى:{" "}
-                    <span className="en-text" dir="ltr">
-                      {endDate}
-                    </span>
-                  </h6>
-                </div>
-              </div>
-            </div>
-            <div className="w-10 d-flex flex-column align-items-center justify-content-center">
-              {discussion.active == true ? (
-                <span className="badge light-bg text-white">مفتوح</span>
-              ) : (
-                <span className="badge red-bg text-white">مغلق</span>
-              )}
-            </div>
-          </Link>
-        );
-      });
-    }
-  }
+//     if (discussions) {
+//       return discussions.map(discussion => {
+//         const startsAt = new Date(discussion.startsAt);
+//         const startDate = startsAt.toLocaleString();
+//         const endsAt = new Date(discussion.endsAt);
+//         const endDate = endsAt.toLocaleString();
+//         return (
+//           <Link
+//             to={`/course/content/${courseId}/discussions/${discussion.id}`}
+//             className="discussion-item d-flex align-items-center"
+//           >
+//             <div
+//               className="media w-90 d-flex align-items-center justify-content-between"
+//               key={discussion.id}
+//             >
+//               <div className="media-body">
+//                 <h6 className="dark-text">{discussion.title}</h6>
+//                 <div className="d-flex align-items-center">
+//                   <h6 className="dark-silver-text smaller mb-0 mr-3">
+//                     من:{" "}
+//                     <span className="en-text" dir="ltr">
+//                       {startDate}
+//                     </span>
+//                   </h6>
+//                   <h6 className="dark-silver-text smaller mb-0">
+//                     إلى:{" "}
+//                     <span className="en-text" dir="ltr">
+//                       {endDate}
+//                     </span>
+//                   </h6>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="w-10 d-flex flex-column align-items-center justify-content-center">
+//               {discussion.active == true ? (
+//                 <span className="badge light-bg text-white">مفتوح</span>
+//               ) : (
+//                 <span className="badge red-bg text-white">مغلق</span>
+//               )}
+//             </div>
+//           </Link>
+//         );
+//       });
+//     }
+//   }
 
-  render() {
-    return (
-      <>
-      {this.state.isPageLoading ? (
-        <div
-        className="silver-bg box-layout w-100 pb-0 p-3 mt-4 d-flex justify-content-center align-items-center"
-        style={{ minHeight: 350 }}
-      >
-        <Loader type="ball-spin-fade-loader" className="dark-loader" />
-      </div>
-      ) : (
-      <React.Fragment>
-        <div className="row mb-4 no-gutters">
-          <div className="col-12">
-            <div className="d-flex justify-content-between align-items-center responsive-col">
-              <h6 className="dark-text small mb-0 mt-0">المناقشات</h6>
-            </div>
-          </div>
-        </div>
+//   render() {
+//     return (
+//       <>
+//       {this.state.isPageLoading ? (
+//         <div
+//         className="silver-bg box-layout w-100 pb-0 p-3 mt-4 d-flex justify-content-center align-items-center"
+//         style={{ minHeight: 350 }}
+//       >
+//         <Loader type="ball-spin-fade-loader" className="dark-loader" />
+//       </div>
+//       ) : (
+//       <React.Fragment>
+//         <div className="row mb-4 no-gutters">
+//           <div className="col-12">
+//             <div className="d-flex justify-content-between align-items-center responsive-col">
+//               <h6 className="dark-text small mb-0 mt-0">المناقشات</h6>
+//             </div>
+//           </div>
+//         </div>
 
-        <div className="row no-gutters">
-          <div className="col-12">
-            {this.state.discussions == undefined ||
-            this.state.discussions.length == 0 ? (
-              <React.Fragment>
-                <div
-                  className="box-layout shadow-sm d-flex flex-column w-100 rounded p-4 justify-content-center align-items-center"
-                  style={{ height: 300 }}
-                >
-                  <img
-                    src={
-                      process.env.PUBLIC_URL +
-                      "/assets/images/empty-discussions.png"
-                    }
-                    height="90"
-                    className="contain-img mb-3"
-                  />
-                  <p className="dark-text mt-0 mb-0">لا يوجد مناقشات متاحة </p>
-                </div>
-              </React.Fragment>
-            ) : (
-              <div className="box-layout shadow-sm">
-                {this.renderDiscussions()}
-              </div>
-            )}
-          </div>
-        </div>
-      </React.Fragment>
-       )}
-       </>
-    );
-  }
-}
+//         <div className="row no-gutters">
+//           <div className="col-12">
+//             {this.state.discussions == undefined ||
+//             this.state.discussions.length == 0 ? (
+//               <React.Fragment>
+//                 <div
+//                   className="box-layout shadow-sm d-flex flex-column w-100 rounded p-4 justify-content-center align-items-center"
+//                   style={{ height: 300 }}
+//                 >
+//                   <img
+//                     src={
+//                       process.env.PUBLIC_URL +
+//                       "/assets/images/empty-discussions.png"
+//                     }
+//                     height="90"
+//                     className="contain-img mb-3"
+//                   />
+//                   <p className="dark-text mt-0 mb-0">لا يوجد مناقشات متاحة </p>
+//                 </div>
+//               </React.Fragment>
+//             ) : (
+//               <div className="box-layout shadow-sm">
+//                 {this.renderDiscussions()}
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//       </React.Fragment>
+//        )}
+//        </>
+//     );
+//   }
+// }
