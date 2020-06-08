58cafd62909583a787b217c39d0894e07c79fac0
Saif Abushamma <sabushamma@terkwaz.com>
2020-06-08 20:04:18 +0300

    STUD-71 WIP: withdraw course transactions

--- src/components/account/billings/billing-list.js
+++ src/components/account/billings/billing-list.js
@@ -18,7 +18,9 @@ class BillingListComponent extends Component {
     isRefundOpen: false,
     isWrongTransactionOpen: false,
     details: [],
-    remainingAmount : null
+    withdraws :[],
+    remainingAmount : null,
+
   };
 
   openRefundModal = () => {
@@ -57,6 +59,14 @@ class BillingListComponent extends Component {
       .catch(error => {
         console.log(error);
       });
+      axios
+      .get(`${apiBaseUrl}/content/${courseId}/withdraw_history`, { headers })
+      .then(response => {
+        this.setState({ withdraws: response.data.data.refundRequests });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   renderTransaction() {
@@ -85,6 +95,45 @@ class BillingListComponent extends Component {
             </td>
             <td className="light-font-text dark-silver-text small">
               {transaction.status}
+            </td>
+          </tr>
+        );
+      });
+    }
+  }
+
+  renderwithdraw () {
+    const withdraws = this.state.withdraws;
+
+    if (withdraws) {
+      return withdraws.map(withdraw => {
+        const date = new Date(withdraw.createdAt);
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+        var withdrawsDate = year + "-" + month + "-" + day;
+        var hijriDate = moment(withdrawsDate).format("iYYYY/iM/iD");
+
+        return (
+          <tr className="text-center" key={withdraw.id}>
+            <td className="dark-silver-text small">
+              <span className="en-text">{hijriDate}</span>
+            </td>
+            <td scope="row" className="light-font-text dark-silver-text small">
+              {withdraw.bankName}
+            </td>
+            <td className="en-text dark-silver-text small">
+              {withdraw.accountOwnerName}
+            </td>
+            <td className="light-font-text dark-silver-text small">
+              {withdraw.status == "Pending" ? ("قيد المراجعة") :
+               withdraw.status == "Approved" ? ("مقبول") :
+               withdraw.status == "Transferred" ? ("محول") :
+               withdraw.status == "Rejected" ? ("مرفوض") :
+               null }
+            </td>
+            <td className="light-font-text dark-silver-text small">
+              {withdraw.adminNotes}
             </td>
           </tr>
         );
@@ -202,6 +251,56 @@ class BillingListComponent extends Component {
                 </Table>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="container pb-5">
+          <div className="row mb-3 no-gutters">
+            <div className="col-12">
+              <div className="d-flex justify-content-between align-items-center responsive-col">
+                <h6 className="dark-text small mb-0 mt-0">طلبات الانسحاب </h6>
+              </div>
+            </div>
+          </div>
+
+          <div className="row no-gutters">
+          {!this.state.withdraws.length == 0 ? (
+            <div className="col-12">
+              <div className="box-layout shadow-sm">
+                <Table className="mb-0">
+                  <thead className="silver-bg">
+                    <tr className="text-center">
+                      <th className="w-20 dark-silver-text small border-0">
+                      تاريخ إرسال الطلب
+                      </th>
+                      <th className="w-20 dark-silver-text small border-0">
+                      إسم البنك
+                      </th>
+                      <th className="w-20 dark-silver-text small border-0">
+                      مالك الحساب
+                      </th>
+                      <th className="w-20 dark-silver-text small border-0">
+                        الحالة
+                      </th>
+                      <th className="w-20 dark-silver-text small border-0">
+                        ملاحظات الأدارة
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>{this.renderwithdraw()}</tbody>
+                  
+                </Table>
+              </div>
+            </div>
+            ): <React.Fragment>
+            <div className="col-12">
+              <div
+                className="d-flex flex-column align-items-center justify-content-center shadow-sm silver-bg"
+                style={{ height: 80, background:"#F2F2F2" }}
+              >
+                <h5 className="dark-silver-text mt-0">لا يوجد طلبات انسحاب</h5>
+              </div>
+            </div>
+          </React.Fragment>}
           </div>
         </div>
       </React.Fragment>
