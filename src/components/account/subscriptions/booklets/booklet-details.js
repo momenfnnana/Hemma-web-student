74007c9c5960cbd7d3db1410c30a8e6ce448af50
Saif Abushamma <sabushamma@terkwaz.com>
2020-07-05 18:04:54 +0300

    STUD-182 Add watermark to booklet

--- src/components/account/subscriptions/booklets/booklet-details.js
+++ src/components/account/subscriptions/booklets/booklet-details.js
@@ -3,8 +3,21 @@ import { getUser } from "../../../../actions/user.actions";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import "../styles.sass";
+import { Page, pdfjs } from 'react-pdf';
+import { Document } from 'react-pdf/dist/entry.webpack';
+import ReactToPrint from "react-to-print";
+pdfjs.GlobalWorkerOptions.workerSrc =
+  "//mozilla.github.io/pdf.js/build/pdf.worker.js";
+
 
 export class BookletDetailsComponent extends Component {
+  state = {
+    numPages: null,
+  }
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
   downloadPdf = () => {
     fetch(this.props.location.state.url, {
       method: "GET",
@@ -18,7 +31,7 @@ export class BookletDetailsComponent extends Component {
         var blob = response;
         var reader = new window.FileReader();
         reader.readAsDataURL(blob);
-        reader.onloadend = function() {
+        reader.onloadend = function () {
           var base64data = reader.result;
           window.open(base64data);
         };
@@ -30,6 +43,18 @@ export class BookletDetailsComponent extends Component {
   render() {
     const url = this.props.location.state.url;
     const name = this.props.location.state.name;
+    const user = this.props && this.props.user;
+    const options = {
+      chunkWidth: 150,
+      chunkHeight: 60,
+      textAlign: 'left',
+      textBaseline: 'bottom',
+      globalAlpha: 0.20,
+      font: '14px Microsoft Yahei',
+      rotateAngle: -0.26,
+      fillStyle: '#666'
+    }
+    const { numPages } = this.state;
     return (
       <React.Fragment>
         <div className="row no-gutters">
@@ -39,14 +64,49 @@ export class BookletDetailsComponent extends Component {
             </div>
           </div>
           <div className="col-12">
-            <embed
-              src={`${url}`}
-              type="application/pdf"
-              width="100%"
-              height="1000"
-            />
-          </div>
-        </div>
+            <div className="box-layout shadow-sm w-100 gray-box-border scrollable-box">
+              <div className="pdf-wrapper d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center">
+                  <ReactToPrint
+                    trigger={() => (
+                      <div className="white-border bg-transparent rounded d-flex align-items-center justify-content-center p-1 clickable ml-3">
+                        <img
+                          src={
+                            process.env.PUBLIC_URL +
+                            "/assets/images/printer.png"
+                          }
+                          height="25"
+                          className="contain-img clickable"
+                        />
+                      </div>
+                    )}
+                    content={() => this.componentRef}
+                  />
+                </div>
+              </div>
+              <div className="m-4">
+                <Document
+                  file={`${url}`}
+                  onLoadSuccess={this.onDocumentLoadSuccess}
+                  ref={el => (this.componentRef = el)}
+                >
+                  {Array.from(new Array(numPages), (el, index) => (
+                    <React.Fragment>
+                      <Page key={`page_${index + 1}`} pageNumber={index + 1}>
+                        <div className="watermark w-100 d-flex align-items-center justify-content-between">
+                          <h6 className="mb-0">{user && user.name}</h6>
+
+                          <h6 className="en-text mb-0">
+                            0{user && user.phoneNumber}
+                          </h6>
+                        </div>
+                      </Page>
+                    </React.Fragment>
+                  ))}
+                </Document>
+              </div>
+            </div>
+          </div></div>
       </React.Fragment>
     );
   }
