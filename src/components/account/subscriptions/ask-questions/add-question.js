ca60c10f747b324b3e74d76ae22417a64c73bc6c
Saif Abushamma <sabushamma@terkwaz.com>
2020-07-23 11:17:07 +0300

    STUD-190 WIP: Ask Questions

--- src/components/account/subscriptions/ask-questions/add-question.js
+++ src/components/account/subscriptions/ask-questions/add-question.js
@@ -1,7 +1,58 @@
 import React, { Component } from "react";
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Input, Label } from "reactstrap";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Input,
+  Label,
+} from "reactstrap";
+import Axios from "axios";
+import { apiBaseUrl } from "../../../../api/helpers";
 
 export default class AddQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    let value = event.target.value;
+    this.setState({ question: value });
+  };
+
+  handleSubmit(event) {
+    // const courseId = this.props.match.params.id;
+    let token = localStorage.getItem("token");
+    let headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    let data = {
+      type: "Text",
+      content: this.state.question,
+    };
+    Axios.post(
+      `${apiBaseUrl}/AskQuestions?courseId=4a54634e-907f-4236-8382-b803b3fd7363`,
+      data,
+      {
+        headers,
+      }
+    )
+      .then(() => {
+        // this.props.history.push(
+        //   `/course/content/${courseId}/askQuestions/list`
+        // );
+        console.log("question added");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <React.Fragment>
@@ -10,51 +61,51 @@ export default class AddQuestion extends Component {
           toggle={this.props.toggleModal}
           className="modal-dialog-centered"
         >
-          <ModalHeader toggle={this.props.toggleModal} className="bg-light">
-            <h6 className="dark-text small mb-0 mt-0">اضافة سؤال </h6>
-          </ModalHeader>
-          <ModalBody>
+          <form onSubmit={this.handleSubmit}>
+            <ModalHeader toggle={this.props.toggleModal} className="bg-light">
+              <h6 className="dark-text small mb-0 mt-0">اضافة سؤال </h6>
+            </ModalHeader>
+            <ModalBody>
               <Label>
-                  <h6 className="dark-text small mb-0 mt-0">السؤال </h6>
-                  </Label>
+                <h6 className="dark-text small mb-0 mt-0">السؤال </h6>
+              </Label>
 
               <div className="position-relative">
-            <textarea
-              id="commentInput"
-              type="text"
-              name="comment"
-              onChange={this.handleChange}
-              placeholder="الرجاء ادخال السؤال"
-              rows="6"
-              className="form-control small dark-text shadow-sm mb-3"
-              ref={ref => (this.commentInput = ref)}
-            />
-            <div className="textarea-icon d-flex align-items-center">
-              <label htmlFor="uploadImage" className="mb-0">
-                <input
-                  className="d-none"
-                  id="uploadImage"
-                  type="file"
-                  onChange={this.handleFileChange}
+                <textarea
+                  id="content"
+                  type="text"
+                  name="content"
+                  onChange={this.handleChange}
+                  placeholder="الرجاء ادخال السؤال"
+                  rows="6"
+                  className="form-control small dark-text shadow-sm mb-3"
                 />
-                <img
-                  src={
-                    process.env.PUBLIC_URL +
-                    "/assets/images/picture.png"
-                  }
-                  height="30"
-                  width="30"
-                  className="contain-img clickable"
-                />
-              </label>
-            </div>
-          </div>
-          </ModalBody>
-          <ModalFooter>
-          <button
-                    className="btn dark-outline-btn w-25"
-                >اضافة</button>
-          </ModalFooter>
+                <div className="textarea-icon d-flex align-items-center">
+                  <label htmlFor="uploadImage" className="mb-0">
+                    <input
+                      className="d-none"
+                      id="uploadImage"
+                      type="file"
+                      onChange={this.handleFileChange}
+                    />
+                    <img
+                      src={
+                        process.env.PUBLIC_URL + "/assets/images/picture.png"
+                      }
+                      height="30"
+                      width="30"
+                      className="contain-img clickable"
+                    />
+                  </label>
+                </div>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <button className="btn dark-outline-btn w-25" type="submit">
+                اضافة
+              </button>
+            </ModalFooter>
+          </form>
         </Modal>
       </React.Fragment>
     );
--- src/components/account/subscriptions/ask-questions/ask-questions-list.js
+++ src/components/account/subscriptions/ask-questions/ask-questions-list.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import { Table, TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
 import { withRouter, Link } from "react-router-dom";
-import classnames from 'classnames';
+import classnames from "classnames";
 import AddQuestion from "./add-question";
-
+import axios from "axios";
+import { apiBaseUrl } from "../../../../api/helpers";
 
 class AskQuestionsListComponent extends Component {
-    state = {
-        isِAddQuestionOpen: false,
-        details: [],
-        allQuestions: [],
-        activeTab: '1'
-      };
+  state = {
+    isِAddQuestionOpen: false,
+    allQuestions: [],
+    activeTab: "1",
+  };
 
   toggle(tab) {
     if (this.state.activeTab !== tab) {
@@ -28,70 +28,94 @@ class AskQuestionsListComponent extends Component {
 
   toggleModal = () => {
     this.setState({ isِAddQuestionOpen: !this.state.isِAddQuestionOpen });
-};
+  };
 
-  renderMyQuestions() {
-        return (
-        <>
-          <tr className="text-center" >
-                <td className="ar-text dark-silver-text small">
-                <span className="ar-text">سؤال 1</span>
-                </td>
-                <td className="dark-silver-text small">
-                <span className="ar-text">تحت المراجعة</span>
-                </td>
-                <td className="light-font-text dark-silver-text small">
-                <Link
-                  className="btn dark-outline-btn w-50"
-                to="/course/content/askQuestions/details"
-              >
-                  تفاصيل
-              </Link>
-                </td>
-         </tr>
-                <tr className="text-center" >
-                <td className="ar-text dark-silver-text small">
-                <span className="ar-text">سؤال 2</span>
-                 </td>
-                <td className="dark-silver-text small">
-                    <span className="ar-text">تمت الاجابة</span>
-                </td>
-                <td className="light-font-text dark-silver-text small">
-                    <Link
-                  className="btn dark-outline-btn w-50"
-                to="/course/content/askQuestions/details"
-              >
-                  تفاصيل
-              </Link>
-                </td>
-                </tr>
-          </>
-        );
-    }
-
-  renderAllQuestions() {
-      return (
-          <tr className="text-center">
-              <td className="en-text dark-silver-text small">
-                  <span className="ar-text">سؤال 3</span>
-              </td>
-              <td className="dark-silver-text small">
-                  <span className="ar-text">تمت الاجابة</span>
-              </td>
-              <td className="light-font-text dark-silver-text small">
-                  {/* <button
-                      className="btn dark-outline-btn w-50"
-                  >تفاصيل</button> */}
-                  <Link
-                  className="btn dark-outline-btn w-50"
-                to="/course/content/askQuestions"
-              >تفاصيل</Link>
-              </td>
-          </tr>
-      );
+  componentDidMount() {
+    const courseId = this.props.match.params.id;
+    let token = localStorage.getItem("token");
+    let headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    axios
+      .get(`${apiBaseUrl}/AskQuestions/me?courseId=${courseId}`, { headers })
+      .then((response) => {
+        this.setState({ myQuestions: response.data.data.data });
+        console.log(response.data.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    axios
+      .get(`${apiBaseUrl}/AskQuestions?courseId=${courseId}`, { headers })
+      .then((response) => {
+        this.setState({ allQuestions: response.data.data.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
+  renderMyQuestions() {
+    const myQuestions = this.state.myQuestions;
+    if (myQuestions) {
+      return myQuestions.map((myQuestion) => {
+        return (
+          <>
+            <tr className="text-center">
+              <td className="ar-text dark-silver-text small">
+                <span className="ar-text">{myQuestion.content}</span>
+              </td>
+              <td className="dark-silver-text small">
+                <span className="ar-text">
+                  {myQuestion.isAnswered === true
+                    ? "تمت الاجابة"
+                    : "قيد المراجعة"}
+                </span>
+              </td>
+              <td className="light-font-text dark-silver-text small">
+                <Link
+                  className="btn dark-outline-btn w-50"
+                  to="/course/content/askQuestions/details"
+                >
+                  تفاصيل
+                </Link>
+              </td>
+            </tr>
+          </>
+        );
+      });
+    }
+  }
 
+  renderAllQuestions() {
+    const allQuestions = this.state.allQuestions;
+    if (allQuestions) {
+      return allQuestions.map((allQuestion) => {
+        return (
+          <tr className="text-center">
+            <td className="en-text dark-silver-text small">
+              <span className="ar-text">{allQuestion.content}</span>
+            </td>
+            <td className="dark-silver-text small">
+              <span className="ar-text">
+                {allQuestion.isAnswered === true
+                  ? "تمت الاجابة"
+                  : "قيد المراجعة"}
+              </span>
+            </td>
+            <td className="light-font-text dark-silver-text small">
+              <Link
+                className="btn dark-outline-btn w-50"
+                to="/course/content/askQuestions/details"
+              >
+                تفاصيل
+              </Link>
+            </td>
+          </tr>
+        );
+      });
+    }
+  }
   render() {
     return (
       <React.Fragment>
@@ -99,50 +123,51 @@ class AskQuestionsListComponent extends Component {
           <Nav tabs>
             <NavItem>
               <NavLink
-                className={`${classnames({ active: this.state.activeTab === '1' })} ${"clickable"}`}
-                onClick={() => { this.toggle('1'); }}>
+                className={`${classnames({
+                  active: this.state.activeTab === "1",
+                })} ${"clickable"}`}
+                onClick={() => {
+                  this.toggle("1");
+                }}
+              >
                 <h6 className="dark-text small mb-0 mt-0">اسئلتي </h6>
               </NavLink>
             </NavItem>
             <NavItem>
               <NavLink
-              className={`${classnames({ active: this.state.activeTab === '2' })} ${"clickable"}`}
-                onClick={() => { this.toggle('2'); }}>
+                className={`${classnames({
+                  active: this.state.activeTab === "2",
+                })} ${"clickable"}`}
+                onClick={() => {
+                  this.toggle("2");
+                }}
+              >
                 <h6 className="dark-text small mb-0 mt-0">جميع الأسئلة </h6>
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink
-                className="clickable"
-                onClick={() => this.toggleModal}>
-                <h6
-                  className="dark-text small"
-                  onClick={this.toggleModal}
-                >
+              <NavLink className="clickable" onClick={() => this.toggleModal}>
+                <h6 className="dark-text small" onClick={this.toggleModal}>
                   <img
-                    src={
-                      process.env.PUBLIC_URL +
-                      "/assets/images/question.png"
-                    }
+                    src={process.env.PUBLIC_URL + "/assets/images/question.png"}
                     height="20"
                     width="20"
                     className="mr-2 contain-img"
                   />
-                    اضافة سؤال
-                  </h6>
+                  اضافة سؤال
+                </h6>
                 <AddQuestion
-                    toggleModal={this.toggleModal}
-                    isِAddQuestionOpen={this.state.isِAddQuestionOpen}
+                  toggleModal={this.toggleModal}
+                  isِAddQuestionOpen={this.state.isِAddQuestionOpen}
                 />
-
               </NavLink>
             </NavItem>
           </Nav>
           <TabContent activeTab={this.state.activeTab}>
             <TabPane tabId="1">
-              {this.state.activeTab == 1 ?
+              {this.state.activeTab == 1 ? (
                 <div className="row no-gutters">
-                 
+                  {!this.state.myQuestions == 0 ? (
                     <div className="col-12">
                       <div className="box-layout shadow-sm">
                         <Table className="mb-0">
@@ -163,30 +188,32 @@ class AskQuestionsListComponent extends Component {
                         </Table>
                       </div>
                     </div>
-                  
-                      {/* <React.Fragment>
-                        <div className="col-12">
-                          <div
-                            className="d-flex flex-column align-items-center justify-content-center shadow-sm silver-bg"
-                            style={{ height: 80, background: "#F2F2F2" }}
-                          >
-                            <h5 className="dark-silver-text mt-0">لم تقم باضافة اسئله</h5>
-                          </div>
+                  ) : (
+                    <React.Fragment>
+                      <div className="col-12">
+                        <div
+                          className="d-flex flex-column align-items-center justify-content-center shadow-sm silver-bg"
+                          style={{ height: 80, background: "#F2F2F2" }}
+                        >
+                          <h5 className="dark-silver-text mt-0">
+                            لم تقم باضافة اسئله
+                          </h5>
                         </div>
-                      </React.Fragment> */}
-                   
+                      </div>
+                    </React.Fragment>
+                  )}
                 </div>
-                : null}
+              ) : null}
             </TabPane>
             <TabPane tabId="2">
-              {this.state.activeTab == 2 ?
+              {this.state.activeTab == 2 ? (
                 <div className="row no-gutters">
                   {!this.state.allQuestions.length == 0 ? (
                     <div className="col-12">
                       <div className="box-layout shadow-sm">
                         <Table className="mb-0">
                           <thead className="silver-bg">
-                          <tr className="text-center">
+                            <tr className="text-center">
                               <th className="w-25 dark-silver-text small border-0">
                                 السؤال
                               </th>
@@ -199,23 +226,25 @@ class AskQuestionsListComponent extends Component {
                             </tr>
                           </thead>
                           <tbody>{this.renderAllQuestions()}</tbody>
-
                         </Table>
                       </div>
                     </div>
-                  ) : <React.Fragment>
+                  ) : (
+                    <React.Fragment>
                       <div className="col-12">
                         <div
                           className="d-flex flex-column align-items-center justify-content-center shadow-sm silver-bg"
                           style={{ height: 80, background: "#F2F2F2" }}
                         >
-                          <h5 className="dark-silver-text mt-0">لا يوجد أسئلة</h5>
+                          <h5 className="dark-silver-text mt-0">
+                            لا يوجد أسئلة
+                          </h5>
                         </div>
                       </div>
-                    </React.Fragment>}
+                    </React.Fragment>
+                  )}
                 </div>
-
-                : null}
+              ) : null}
             </TabPane>
           </TabContent>
         </div>
@@ -224,5 +253,3 @@ class AskQuestionsListComponent extends Component {
   }
 }
 export const AskQuestionsList = withRouter(AskQuestionsListComponent);
-
-
