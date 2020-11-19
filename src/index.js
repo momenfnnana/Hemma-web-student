ff7aead252248b64b78d61d4aef4d266b578e891
Sarah Abuteen <sabuteen@terkwaz.com>
2020-11-19 12:07:38 +0200

    sentry configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
 import App from "./App";
 import "./index.sass";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+Sentry.init({ dsn: "https://5f93c7937f7a40b2aa750dab4c00653f@o392428.ingest.sentry.io/5239902" });
+
+ReactDOM.render(<Sentry.ErrorBoundary fallback={"An error has occurred"}><App /></Sentry.ErrorBoundary>, document.getElementById("root"));
