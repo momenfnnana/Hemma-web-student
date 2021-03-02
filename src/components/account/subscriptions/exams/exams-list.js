1c7f9064b9ef1c53aa857b05e1e5674a84a432aa
AmiraThabet <amira@hemma.sa>
2021-03-02 17:19:05 +0200

    enable share Exam for free groups only and edit help me

--- src/components/account/subscriptions/exams/exams-list.js
+++ src/components/account/subscriptions/exams/exams-list.js
@@ -77,7 +77,7 @@ createCourseLink(courseId,examid)
               >
                 اختبر الآن
               </Link>
-              <ToastDemo copyLink={{ btnName:'مشاركة الاختبار',link:this.createCourseLink(courseId,exam.id)}} />
+             {/* <ToastDemo copyLink={{ btnName:'مشاركة الاختبار',link:this.createCourseLink(courseId,exam.id)}} />*/}
             </td>
           </tr>
         </React.Fragment>
