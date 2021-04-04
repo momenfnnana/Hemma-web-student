217f43638dc01cad99f8c2cea6ee18666a839c61
Mina Aead <Mina@hemma.sa>
2021-04-04 17:25:52 +0200

    Main page new design apply

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Integrations } from "@sentry/tracing";
 import App from "./App";
 import "./index.sass";
 
+
 Sentry.init({
     dsn: "https://2fde36f1c2fb405db43b8cc9e5f61e8f@o392428.ingest.sentry.io/5536321",
     integrations: [
