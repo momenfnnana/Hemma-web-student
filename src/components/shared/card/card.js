8a553b30c2e4abb076bb3069b65b87b5979becd7
Sameh EL-Kady <sameh.elkady@z2data.com>
2021-04-07 15:55:11 +0200

    apply end course date

--- src/components/shared/card/card.js
+++ src/components/shared/card/card.js
@@ -50,85 +50,16 @@ class CardComponent extends Component {
     ));
     return (
       <React.Fragment>
-        {/* <Link to={`/course/details/${course.slug}`} key={course.id}>
-          <div className="card course-card shadow-sm m-2 border-0" dir="rtl">
-            <header className="card-thumb">
-              <img key={course.id} src={course.bannerUrl} alt={course.nameAr} />
-            </header>
-            <div className="card-body">
-              <h6 className="card-title small mid-text" key={course.id}>
-                {course.nameAr}
-              </h6>
-              {course.relatedDiscound ?
-               ( <h5 className="small light-font-text dark-text">
-                   نقدر لك ولاءك لهمة ورجوعك لنا مره اخرى، لهذا تم تقديم لك خصم خاص 
-                   &nbsp;{course.relatedDiscound} %&nbsp;
-                  بسبب أشتراكك فى  
-                  &nbsp;{course.relatedDiscoundCourse} &nbsp;
-                    </h5>
-              ):null}
-              
-              {course.instructors == undefined ||
-              course.instructors == 0 ? null : (
-                <div className="card-subtitle">
-                  <div className="row">{instructor}</div>
-                </div>
-              )}
-              <ul className="list-inline mb-1">
-                <li className="list-inline-item light-font-text small dark-text mr-4 d-inline-flex align-items-center">
-                  <img
-                    src={process.env.PUBLIC_URL + "/assets/images/calendar.png"}
-                    height="12"
-                    width="12"
-                    className="mr-1"
-                  />
-                  <span className="en-text mr-1">
-                    {parseFloat(course.durationInHours)}
-                  </span>
-                  ساعة
-                </li>
-                <li className="list-inline-item light-font-text small dark-text en-text d-inline-flex align-items-center">
-                  <img
-                    src={process.env.PUBLIC_URL + "/assets/images/quarters.png"}
-                    height="12"
-                    width="12"
-                    className="mr-1"
-                  />
-                  <span className="en-text">{hijriDate}</span>{" "}
-                </li>
-                {course.originalPrice ? (
-                  <>
-                    <li className="list-inline-item light-font-text small red-text d-inline-flex align-items-center float-right">
-                      <span className="en-text mr-1">
-                        {course.price && parseFloat(course.price.toFixed(2))}
-                      </span>
-                      ريال
-                    </li>
-                    <li className="list-inline-item light-font-text small d-inline-flex align-items-center float-right mr-1 crossed-line dark-text">
-                      <span className="en-text mr-1">
-                        {course.originalPrice &&
-                          parseFloat(course.originalPrice.toFixed(2))}
-                        <span className="ar-text mr-1">ريال</span>
-                      </span>
-                    </li>
-                  </>
-                ) : (
-                  <li className="list-inline-item light-font-text small dark-text d-inline-flex align-items-center float-right mr-1">
-                    <span className="en-text mr-1">
-                      {course.price && parseFloat(course.price.toFixed(2))}
-                    </span>
-                    ريال
-                  </li>
-                )}
-              </ul>
-            </div>
-            <p className="card-description dark-text light-font-text smaller mb-0 text-center text-break">
-              {desc}
-            </p>
-          </div>
-        </Link> */}
-        
-           <Link to={`/course/details/${course.slug}`} key={course.id} className="main-color">
+          <div className="card course-card shadow-sm m-2 border-0" dir="rtl" onClick={() => {
+               new Date(course.endsAt) > new Date() ?  history.push(`/course/details/${course.slug}`):
+               swal(
+                   "عفواً",
+                   "الدورة مغلقة، لا يمكنك شراؤها وبإمكانك التواصل مع الدعم الفني في حال أردت",
+                   "error",
+                  {
+                    button: "متابعة"
+                  }
+                  ) }} >
          
                       <div className="card p-3 border-dashed card-ele max-height-380 min-height-380 position-relative mb-6">
                         <div className="card-img">
@@ -153,7 +84,7 @@ class CardComponent extends Component {
                           </div> */}
                         </div>
                       </div>
-                      </Link>
+                      </div>
                    
       </React.Fragment>
     );
