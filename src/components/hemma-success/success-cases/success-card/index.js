b417835b6fd0a92c06d9a8181b43dd948a2c6caf
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2021-06-10 16:19:16 +0300

    ui enhancments for booklet page

--- src/components/hemma-success/success-cases/success-card/index.js
+++ src/components/hemma-success/success-cases/success-card/index.js
@@ -1,27 +1,35 @@
 import React from "react";
+import TrimmedText from "../../../../shared-components/trimmed-text";
 import "./index.scss";
 
 const Rating = ({ successCase }) => {
   console.log({});
-  return(
-  <div class="card-body px-2 py-2">
-    <p class="d-flex align-items-center light-gray mb-1 font-size-13">
-      <span class="d-block main-color-light mr-2">اسم الطالب : </span>
-      {successCase?.rating?.studentName}
-    </p>
-    <p class="light-gray font-size-13 m-0">
-      <span class="main-color-light">التقييم : </span>
-      {successCase?.rating?.feedBack}
-    </p>
-  </div>
-);}
+  return (
+    <div class="card-body px-2 py-2">
+      <p class="d-flex align-items-center light-gray mb-1 font-size-13">
+        <span class="d-block main-color-light mr-2">اسم الطالب : </span>
+        {successCase?.rating?.studentName}
+      </p>
+      <p class="light-gray font-size-13 m-0">
+        <span class="main-color-light">التقييم : </span>
+        {successCase?.rating?.feedBack}
+      </p>
+    </div>
+  );
+};
 
 const contentReducer = (successCase) => {
   switch (successCase?.source) {
     case "Media":
-      return <a href={successCase?.url}>
-        <img src={successCase?.img} className="w-100 height-70" style={{height:'170px !important'}} />
-      </a>;
+      return (
+        <a href={successCase?.url}>
+          <img
+            src={successCase?.img}
+            className="w-100 height-auto w-100"
+            style={{ height: "170px !important" }}
+          />
+        </a>
+      );
     case "Rating":
       return <Rating successCase={successCase} />;
     default:
@@ -31,17 +39,16 @@ const contentReducer = (successCase) => {
 
 export const SuccessCard = ({ successCase }) => {
   return (
-    <div class="col-lg-4">
-      <div class="status-card sider-items min-height-150">
-        <div class="quote-icon">
-          <i class="fas fa-quote-left"></i>
+    <div class="">
+      <div class="status-card sider-items h-100 d-flex flex-column">
+        <div class="quote-icon d-flex mx-2">
+          <h6 class="h6 main-color-light text-center mb-3">
+            <TrimmedText text={successCase?.courseName} maxLength={18} />
+          </h6>
+          <i class="fas fa-quote-left mx-2"></i>
         </div>
-        <h6 class="h6 main-color-light text-center mb-3">
-          {successCase?.courseName}
-        </h6>
-        <div class="card">
-          {contentReducer(successCase)}
-      </div>
+
+        <div class="card flex-1">{contentReducer(successCase)}</div>
       </div>
     </div>
   );
