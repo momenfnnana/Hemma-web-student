81aa42ffc601fab076af4c014272c378b2d721c5
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2021-07-18 10:48:07 +0300

    hdie electronic question course section in professional license auto selecet first option

--- src/components/categories/professional-license/courses/course/course-tabs.js
+++ src/components/categories/professional-license/courses/course/course-tabs.js
@@ -3,8 +3,8 @@ export const courseTabs = [
     title: "دورة الرخصة الشاملة",
     id: "Original",
   },
-  {
-    title: "دورة الأسئلة الاكترونية",
-    id: "QuestionsPackage",
-  },
+  // {
+  //   title: "دورة الأسئلة الاكترونية",
+  //   id: "QuestionsPackage",
+  // },
 ];
