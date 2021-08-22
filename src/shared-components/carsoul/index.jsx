8ad13cd721abb5f4aa98dd41b3a5f7002d6d2446
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2021-08-22 16:07:48 +0300

    refactor glide slider to be more generic

--- src/shared-components/carsoul/index.jsx
+++ src/shared-components/carsoul/index.jsx
@@ -1,35 +1,53 @@
-import Glide from '@glidejs/glide'
-import React ,{ useState, useEffect } from 'react'
+import Glide from "@glidejs/glide";
+import React, { useState, useEffect } from "react";
+import "@glidejs/glide/dist/css/glide.core.min.css";
 
-export const Carousel = ({element = 'glide', options, children}) => {
-    const [slider] = useState(new Glide(`.${element}`, options))
-    useEffect(() => {
+const enabledCarsoulOptions = {
+  type: "carousel",
+  startAt: 1,
+  perView: 3,
+  focusAt: "center",
+  gap: 20,
+  autoplay: 4000,
+  animationDuration: 800,
+  hoverpause: false,
+  keyboard: true,
+  direction: "rtl",
+  breakpoints: {
+    1200: { perView: 3 },
+    992: { perView: 2 },
+    768: { perView: 1 },
+  },
+};
+export const Carousel = ({
+  element = "glide",
+  elems = [],
+  SliderComponent
+}) => {
 
-        if(!element) return
-       slider.mount()
+  useEffect(() => {
+    let glide = new Glide(".glide", enabledCarsoulOptions);
+    if (!elems.length) return;
+    if (!elems.length) return;
+    glide.destroy();
+    setTimeout(() => {
+      glide.mount();
+    }, 500);
+  }, [elems?.length]);
 
-      // subscribe to an event 
-      slider.on('run.before', (event) => {
-          // ... do something cool here
-        })
-
-      // cleanup when unmounting component
-      return () => slider.destroy()
-    }, [element])
-
-   return (
-       <div className={element}>
-           <div className="glide__track" data-glide-el="track">
-                  <ul className="glide__slides">
-                    {children.map((slide, index) => {
-                            return React.cloneElement(slide, {
-                                key: index,
-                                className: `${slide.props.className} glide__slide`
-                            })
-                        })
-                    }
-                </ul>
-           </div>
+  return (
+    <div className="container-fluid py-5">
+      <div className="d-flex mx-auto">
+        <div class="glide">
+          <div class="glide__track" data-glide-el="track">
+            <ul class="glide__slides">
+              {elems.map((elem) => (
+                <li className="glide__slide"> <SliderComponent data={elem} /> </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
