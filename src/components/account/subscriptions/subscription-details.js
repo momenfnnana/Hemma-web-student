a7ec1c61cfee333750543ef3ac0ff81addcba1cd
Ahmed_Monier <ahmad1@hemma.sa>
2021-12-05 22:24:58 +0200

    HEMMA-1484 Merged pull request Merged in HEMMA-1484 (pull request #464) 1c7832f

--- src/components/account/subscriptions/subscription-details.js
+++ src/components/account/subscriptions/subscription-details.js
@@ -181,7 +181,7 @@ class SubscriptionDetailsComponent extends Component {
                           subscription={subscription}
                           courseId={courseId}
                         />
-                        {ratingStatus === "Skipped" && (
+                        {/* {ratingStatus === "Skipped" && (
                           <button
                             className="btn light-btn w-100 mb-3"
                             onClick={() =>
@@ -190,8 +190,8 @@ class SubscriptionDetailsComponent extends Component {
                           >
                             قيّم الدورة
                           </button>
-                        )}
-                        <RatingModal
+                        )} */}
+                        {/* <RatingModal
                           isRatingModalOpen={
                             ratingStatus === "Available" ||
                             this.state.forceOpenRatingModal
@@ -201,7 +201,7 @@ class SubscriptionDetailsComponent extends Component {
                           }
                           status={ratingStatus}
                           courseId={courseId}
-                        />
+                        /> */}
                         <Instructors id={courseId} />
                       </div>
                     )}
