13b5d49349940de5ed9cc2ecc388fe22b70a6969
Momen Fnnana <mommen@hemma.sa>
2021-12-12 14:31:13 +0200

    change home help hint

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -688,15 +688,15 @@ renderSucces()
         {!this.state.isNationalDay && <div className='banner banner-program'>
            <div className="container program-container py-4 ">
              <div>
-               <p>عزيزي المشترك في حال واجهتك مشكلة في انشاء الحساب الرجاء التواصل على <a href='https://hemma.sa/?support'>الدعم الفني</a> او على</p>
+               <p className='d-flex justify-contet-center align-items-center'>عزيزي المشترك في حال واجهتك مشكلة في انشاء الحساب الرجاء التواصل على <a href='https://hemma.sa/?support'>الدعم الفني</a> او على<div className='rounded-circle btn-success mx-2 p-2 cursor-poiner' onClick={()=>window.open('http://wa.me/966539412412','_blank')}><FaWhatsapp size={40} /></div></p>
                {/* <h2>  برنامج
                         <span className='program-name'> معلم كفؤ </span>             الإثرائي والمجاني
                </h2>
                <p> صمم هذا البرنامج بعناية مع نخبة من الخبراء ليحقق احتياج المعلمين في تطوير مهاراتهم التدريسية </p> */}
              </div>
-             <div onClick={()=>window.open('http://wa.me/966539412412','_blank')}>
-               <button type="button" className="btn btn-success join-now d-flex justify-content-center align-items-center">الواتساب <FaWhatsapp className='mx-1' size={30} /></button>
-             </div>
+             {/* <div onClick={()=>window.open('http://wa.me/966539412412','_blank')}>
+               <button type="button" className="btn btn-success join-now d-flex justify-content-center align-items-center"><FaWhatsapp className='mx-1' size={40} /></button>
+             </div> */}
            </div>
          </div>}
       {/* <!-- Start The Main Banner Text --> */}
