2608cd9c16c741aca4769a0d6298df97639a823e
Momen Fnnana <mommen@hemma.sa>
2021-12-28 10:31:20 +0200

    fx issue with reading design type in course training questions

--- src/components/account/subscriptions/training/training-list.js
+++ src/components/account/subscriptions/training/training-list.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { Table } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import axios from "axios";
 import { apiBaseUrl } from "../../../../api/helpers";
 import Loader from "react-loaders";
 import "loaders.css/src/animations/ball-spin-fade-loader.scss";
 import { ToastDemo } from "../../../categories/quick-questions/toast-notification";
+import { connect } from "react-redux";
 
-export class TrainingList extends Component {
+class TrainingList extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -25,11 +26,13 @@ export class TrainingList extends Component {
         let url="";
         if(this.props.designType)
         {
+          console.log({designType:this.props.designType});
         url = new URL(`${apiBaseUrl}/Exams?courseId=${courseId}`);
         }
         else
         {
-          url = new URL(`${apiBaseUrl}/Exams?courseId=${courseId}`);
+          console.log({designType:this.props.designType});
+          url = new URL(`${apiBaseUrl}/Exams/All?courseId=${courseId}`);
         }
         url.searchParams.append("type", "Training");
         axios
@@ -151,3 +154,19 @@ export class TrainingList extends Component {
         )
     }
 }
+
+function mapStateToProps(state) {
+  return {
+    subscription: state.subscription,
+    designType: state.user?.designType,
+    user: state.user,
+  };
+}
+const mapDispatchToProps = {};
+
+TrainingList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TrainingList);
+
+export default withRouter(TrainingList);
