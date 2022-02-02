dd3b344098c4902869777e1dbcf57702806e8140
Momen Fnnana <mommen@hemma.sa>
2022-02-02 10:20:23 +0200

    fix issue whenno authorize to redirect to auth screens

--- src/components/categories/details.js
+++ src/components/categories/details.js
@@ -389,7 +389,7 @@ export class _CategoryDetails extends Component {
         {
           button: "متابعة",
         }
-      );
+      ).then(res=>this.props.history.push("/auth/login"));
     }
   }
 
