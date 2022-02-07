fba765a7399e650cdc7d523c69206bf75a0c2a60
Momen Fnnana <mommen@hemma.sa>
2022-02-07 23:08:27 +0200

    fix issue with showing showProLicense

--- src/components/categories/professional-license/index.js
+++ src/components/categories/professional-license/index.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import SubCourseWrapper from '../sub-course-wrapper'
-import ProfessionalCourses from './courses'
-import "./index.scss"
-import SubscribtionRequest from './subscribtion-request'
+import React from "react";
+import SubCourseWrapper from "../sub-course-wrapper";
+import ProfessionalCourses from "./courses";
+import "./index.scss";
+import SubscribtionRequest from "./subscribtion-request";
 
-export const ProfessionalLicenseText = 'الرخصة المهنية'
+export const ProfessionalLicenseText = "الرخصة المهنية";
 
-export default function ProfessionalLicense({categoryData}) {
-    return (
-        <div className="container professional-license-wrapper">
-            <SubCourseWrapper />
-            <ProfessionalCourses categoryData={categoryData} />
-        </div>
-    )
+export default function ProfessionalLicense({
+  categoryData,
+  showProLicense = false,
+  showGeneralCourse = false,
+  showSpecialityCourse = false,
+}) {
+  return (
+    <div className="container professional-license-wrapper">
+      {showProLicense ? (
+        <>
+          <SubCourseWrapper />
+          <ProfessionalCourses
+            {...{ categoryData, showGeneralCourse, showSpecialityCourse }}
+          />
+        </>
+      ) : null}
+    </div>
+  );
 }
